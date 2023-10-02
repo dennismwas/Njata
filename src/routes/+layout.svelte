fa67858c6c8f05a2759c7852a30234012ffd48a8
Dennoh <dennismwas89@gmail.com>
2023-10-02 12:39:45 +0300

    vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,35 +1,43 @@
 <script>
-	import Footer from './Footer.svelte';
-import Header from './Header.svelte';
-import './styles.css';
+	import Footer from "./Footer.svelte";
+	import Header from "./Header.svelte";
+	import "./styles.css";
+	import { dev } from "$app/environment";
+	import { inject } from "@vercel/analytics";
 
+	inject({ mode: dev ? "development" : "production" });
 
-let data=     [   { name: "description", content: "Njata Country House AirBnB located in Nyahururu Olkalou Road" },
-        {
-            name:"keywords",
-      content:"mile, stone, nyahururu-olkalou, along, polytechnic, national, nyandarua, airbnb,house,country house,nyandarua county,nyanadarua national polytechnic,airbnb house,house for airbnb,airbnb near me,airbnb nakuru,airbnb login,meaning of airbnb,airbnb nairobi,airbnb mombasa,airbnb meaning,country airbnb,airbnb country,airbnb country house, njata, bed and breakfast,bed,breakfast,hotel,njata counmtry house,secure parking,nyanadarua,nyahururu,nyandarua polytechnic,ol'kalou,rooms,guest,deluze rooms,spacious rooms,cctv surveillance,njata country house"
-        },
-        { name: "url", content: "https://njatacountryhouse.co.ke" },
-        {
-            name: "MobileOptimized", content: "width"
-        },
-        { name: "HandheldFriendly", content: "true" },
-		
-        { name:'language' , content:'EN'},
-        { name: "robots", content: "index,follow" },
-        { name:"viewport" ,content:"width=device-width, initial-scale=1.0"},
+	let data = [
+		{
+			name: "description",
+			content:
+				"Njata Country House AirBnB located in Nyahururu Olkalou Road",
+		},
+		{
+			name: "keywords",
+			content:
+				"mile, stone, nyahururu-olkalou, along, polytechnic, national, nyandarua, airbnb,house,country house,nyandarua county,nyanadarua national polytechnic,airbnb house,house for airbnb,airbnb near me,airbnb nakuru,airbnb login,meaning of airbnb,airbnb nairobi,airbnb mombasa,airbnb meaning,country airbnb,airbnb country,airbnb country house, njata, bed and breakfast,bed,breakfast,hotel,njata counmtry house,secure parking,nyanadarua,nyahururu,nyandarua polytechnic,ol'kalou,rooms,guest,deluze rooms,spacious rooms,cctv surveillance,njata country house",
+		},
+		{ name: "url", content: "https://njatacountryhouse.co.ke" },
+		{
+			name: "MobileOptimized",
+			content: "width",
+		},
+		{ name: "HandheldFriendly", content: "true" },
 
+		{ name: "language", content: "EN" },
+		{ name: "robots", content: "index,follow" },
+		{ name: "viewport", content: "width=device-width, initial-scale=1.0" },
 
-        { name: "twitter:title", content: "Njata Country House AirBnB" },
-        { name: "twitter:description", content: "Njata Country House AirBnB" }
-	]
+		{ name: "twitter:title", content: "Njata Country House AirBnB" },
+		{ name: "twitter:description", content: "Njata Country House AirBnB" },
+	];
 </script>
-
 
 <svelte:head>
 	<title>Njata Country House</title>
-	{#each data as dt }
-	<meta name={dt.name} content={dt.content} />
+	{#each data as dt}
+		<meta name={dt.name} content={dt.content} />
 	{/each}
 </svelte:head>
 
@@ -41,5 +49,4 @@ let data=     [   { name: "description", content: "Njata Country House AirBnB lo
 	</main>
 
 	<Footer />
-
 </div>
